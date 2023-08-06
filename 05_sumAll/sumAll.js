a29aaf0cab81e171766493628720335534296a7a
Moha-AlHanbali <moha.alhanbali@gmail.com>
2023-08-06 15:06:19 +0300

    feat: solved sumAll.js

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,13 @@
-const sumAll = function() {
+const sumAll = function (start, end) {
+    if (typeof (start) !== 'number' || typeof (end) !== 'number') return "ERROR";
+    if (start < 0 || end < 0) return "ERROR";
 
+    let total = Math.max(start, end);
+    for (let i = Math.min(start, end); i <= Math.abs(end - start) ; i++) {
+        total += i
+    }
+    
+    return total;
 };
 
 // Do not edit below this line
